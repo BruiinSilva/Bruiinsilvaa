0e7154f1cd55c10cab6f54beff9c4222ae3cc899
Bruno C. Gomes Da Silva <73727546+BruiinSilva@users.noreply.github.com>
2022-08-10 14:46:40 -0300

    PROJETO PARA CONTATOS

---
+++ script.js
@@ -0,0 +1,9 @@
+ // typing text animation script
+ var typed = new Typed(".typing", {
+    strings: [ "Developer", " iniciante" , "Front End"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    cursorChar: ' |',
+    smartBackspace: true,
+    loop: true
+});
